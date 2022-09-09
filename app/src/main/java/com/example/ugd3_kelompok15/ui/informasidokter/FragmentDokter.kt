e6939489725e1a4876cd5fb62661b60a1af903b0
PradnyaKuswara <pradnyakuswara24@gmail.com>
2022-09-10 00:28:18 +0700

    update

--- app/src/main/java/com/example/ugd3_kelompok15/ui/informasidokter/FragmentDokter.kt
+++ app/src/main/java/com/example/ugd3_kelompok15/ui/informasidokter/FragmentDokter.kt
@@ -1,4 +1,33 @@
 package com.example.ugd3_kelompok15.ui.informasidokter
 
-class FragmentDokter {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ugd3_kelompok15.R
+import com.example.ugd3_kelompok15.entity.Dokter
+
+class FragmentDokter : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dokter, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        val adapter : RVDokterAdapter = RVDokterAdapter(Dokter.listOfDokter)
+        val rvDokter : RecyclerView = view.findViewById(R.id.fg_dokter)
+
+        rvDokter.layoutManager = layoutManager
+        rvDokter.setHasFixedSize(true)
+        rvDokter.adapter = adapter
+    }
 }
--- app/src/main/java/com/example/ugd3_kelompok15/ui/informasidokter/RVDokterAdapter.kt
+++ app/src/main/java/com/example/ugd3_kelompok15/ui/informasidokter/RVDokterAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.ugd3_kelompok15.ui.informasidokter
 
-class RVDokterAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ugd3_kelompok15.R
+import com.example.ugd3_kelompok15.entity.Dokter
+
+class RVDokterAdapter(private val data: Array<Dokter>) : RecyclerView.Adapter<RVDokterAdapter.viewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.rv_item_dokter, parent, false)
+        return viewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        val currentItem = data[position]
+        holder.tvNamaDokter.text = currentItem.nama
+        holder.tvSpesialis.text = "${currentItem.spesialis} - ${currentItem.tahunMasuk}"
+        holder.tvJadwal.text = currentItem.hariPraktek
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvNamaDokter: TextView = itemView.findViewById(R.id.tv_namaDokter)
+        val tvSpesialis: TextView = itemView.findViewById(R.id.tv_spesialis)
+        val tvJadwal: TextView = itemView.findViewById(R.id.tv_jadwal)
+    }
 }
