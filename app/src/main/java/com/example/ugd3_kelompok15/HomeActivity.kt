65914bc2446ca1f52fd4afec930e10eed6227366
BeniKarunia <beni.karunia73@gmail.com>
2022-09-10 18:51:49 +0700

    update

--- app/src/main/java/com/example/ugd3_kelompok15/HomeActivity.kt
+++ app/src/main/java/com/example/ugd3_kelompok15/HomeActivity.kt
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import com.example.ugd3_kelompok15.ui.home.FragmentHome
+import com.example.ugd3_kelompok15.ui.informasidokter.FragmentDokter
 import kotlin.system.exitProcess
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var layout_fragment : ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         setTitle("My Hospital")
 
+        changeFragment(FragmentHome())
+//        init()
+//
     }
-
+//    private fun init(){
+//        layout_fragment = findViewById(R.id.layout_fragment)
+//    }
     fun changeFragment(fragment: Fragment?) {
         if (fragment != null) {
             getSupportFragmentManager()
