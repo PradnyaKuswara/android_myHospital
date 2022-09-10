7333104f7b05abe5372c5e12c63ab373b722e1e8
BeniKarunia <beni.karunia73@gmail.com>
2022-09-10 21:15:10 +0700

    update

--- app/src/main/java/com/example/ugd3_kelompok15/ui/home/FragmentHome.kt
+++ app/src/main/java/com/example/ugd3_kelompok15/ui/home/FragmentHome.kt
@@ -1,20 +1,13 @@
 package com.example.ugd3_kelompok15.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
-import com.example.ugd3_kelompok15.LoginActivity
+import androidx.appcompat.app.AlertDialog
 import com.example.ugd3_kelompok15.R
-import com.example.ugd3_kelompok15.ui.faq.FragmentFaq
-import com.example.ugd3_kelompok15.ui.informasidokter.FragmentDokter
-import com.example.ugd3_kelompok15.ui.janjitemu.FragmentJanjiTemu
-import com.example.ugd3_kelompok15.ui.profile.FragmentProfile
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlin.system.exitProcess
 
 class FragmentHome : Fragment() {
@@ -32,13 +25,21 @@ class FragmentHome : Fragment() {
         val btnLogout : Button = view.findViewById(R.id.btnLogout)
 
         btnLogout.setOnClickListener(View.OnClickListener {
-            exitProcess(0)
-        })
-    }
+            getActivity()?.let { it1 ->
+                AlertDialog.Builder(it1).apply {
+                    setTitle("Tolong Konfirmasi")
+                    setMessage("Apakah anda yakin ingin keluar?")
 
-    private fun transitionFragment(fragment: Fragment) {
-        val transition = requireActivity().supportFragmentManager.beginTransaction()
-        transition.replace(R.id.layout_home, fragment)
-            .addToBackStack(null).commit()
+                    setPositiveButton("Iya") { _, _ ->
+                        exitProcess(0)
+                    }
+
+                    setNegativeButton("Tidak"){_, _ ->
+
+                    }
+                    setCancelable(true)
+                }.create().show()
+            }
+        })
     }
 }
