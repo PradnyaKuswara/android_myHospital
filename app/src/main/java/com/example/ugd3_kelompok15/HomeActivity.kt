570785d2563d31c0c801d2e76aa9b35a2e45f3fc
BeniKarunia <beni.karunia73@gmail.com>
2022-09-10 20:52:25 +0700

    update

--- app/src/main/java/com/example/ugd3_kelompok15/HomeActivity.kt
+++ app/src/main/java/com/example/ugd3_kelompok15/HomeActivity.kt
@@ -2,16 +2,22 @@ package com.example.ugd3_kelompok15
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import com.example.ugd3_kelompok15.ui.faq.FragmentFaq
 import com.example.ugd3_kelompok15.ui.home.FragmentHome
 import com.example.ugd3_kelompok15.ui.informasidokter.FragmentDokter
+import com.example.ugd3_kelompok15.ui.janjitemu.FragmentJanjiTemu
+import com.example.ugd3_kelompok15.ui.profile.FragmentProfile
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlin.system.exitProcess
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var layout_fragment : ConstraintLayout
+    private lateinit var navigationBottom : BottomNavigationView
+    private lateinit var textView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,18 +26,50 @@ class HomeActivity : AppCompatActivity() {
         setTitle("My Hospital")
 
         changeFragment(FragmentHome())
-//        init()
-//
+
+        init()
+        navListener()
+
     }
-//    private fun init(){
-//        layout_fragment = findViewById(R.id.layout_fragment)
-//    }
+    private fun init(){
+        textView = findViewById(R.id.textWelcome)
+        navigationBottom = findViewById(R.id.botNavigation)
+    }
     fun changeFragment(fragment: Fragment?) {
         if (fragment != null) {
             getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.layout_fragment, fragment)
                 .commit()
+        }
+    }
+
+
+    private fun navListener() {
+        navigationBottom.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Home -> {
+                   textView.text = item.title
+                    changeFragment(FragmentHome())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.informasi_dokter -> {
+                    textView.text = null
+                    changeFragment(FragmentDokter())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.faq -> {
+                    textView.text = null
+                    changeFragment(FragmentFaq())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.profile -> {
+                    textView.text = null
+                    changeFragment(FragmentProfile())
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
         }
     }
 
--- app/src/main/java/com/example/ugd3_kelompok15/ui/home/FragmentHome.kt
+++ app/src/main/java/com/example/ugd3_kelompok15/ui/home/FragmentHome.kt
@@ -1,18 +1,44 @@
 package com.example.ugd3_kelompok15.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.example.ugd3_kelompok15.LoginActivity
 import com.example.ugd3_kelompok15.R
+import com.example.ugd3_kelompok15.ui.faq.FragmentFaq
+import com.example.ugd3_kelompok15.ui.informasidokter.FragmentDokter
+import com.example.ugd3_kelompok15.ui.janjitemu.FragmentJanjiTemu
+import com.example.ugd3_kelompok15.ui.profile.FragmentProfile
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlin.system.exitProcess
 
 class FragmentHome : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnLogout : Button = view.findViewById(R.id.btnLogout)
+
+        btnLogout.setOnClickListener(View.OnClickListener {
+            exitProcess(0)
+        })
+    }
+
+    private fun transitionFragment(fragment: Fragment) {
+        val transition = requireActivity().supportFragmentManager.beginTransaction()
+        transition.replace(R.id.layout_home, fragment)
+            .addToBackStack(null).commit()
     }
 }
